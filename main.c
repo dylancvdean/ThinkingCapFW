d0d35dc9aebed1a0b6bdf9eee2e4e1ac646e2377
Dylan Dean <dylancvdean@gmail.com>
2025-01-04 21:46:33 -0700

    reenable timer 1, necessary for PWM

--- main.c
+++ main.c
@@ -6,7 +6,6 @@ int main(void){
 	PRR |= (1 << PRTWI); //Disable two wire
 	PRR |= (1 << PRTIM2); //Disable Timer/Counter 2
 	PRR |= (1 << PRTIM0); //Disable Timer/Counter 0
-	PRR |= (1 << PRTIM1); //Disable Timer/Counter 1
 	PRR |= (1 << PRSPI);
 	PRR |= (1 << PRUSART0);
 	DDRB |= (1 << PB5);
