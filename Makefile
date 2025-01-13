25786e88ceb461b39d36df1c5d6384f7787d249e
Dylan Dean <dylancvdean@gmail.com>
2025-01-12 21:14:04 -0700

    13a support

--- Makefile
+++ Makefile
@@ -7,11 +7,17 @@ ifeq ($(CHIP),328p)
 else ifeq ($(CHIP),328pb)
     MCU = atmega328pb
     AVRDUDE_MCU = m328pb
+else ifeq ($(CHIP),t13a)
+    MCU = attiny13a
+    AVRDUDE_MCU = t13
 else
-    $(error Invalid CHIP specified: $(CHIP). Use 328p or 328pb)
+    $(error Invalid CHIP specified: $(CHIP). Use 328p, 328pb, or t13a)
 endif
 
+# Clock frequency
 F_CPU = 1000000
+
+# Compiler and tools
 CC = avr-gcc
 CFLAGS = -mmcu=$(MCU) -DF_CPU=$(F_CPU) -Os
 OBJCPY = avr-objcopy
@@ -20,11 +26,18 @@ PROGRAMMER = usbtiny
 PORT = /dev/ttyUSB0 # Adjust if necessary
 TARGET = main
 
-# Fuse settings (same for both chips)
-LFUSE = 0x62
-HFUSE = 0xDA
-EFUSE = 0xFF
+# Fuse settings
+ifeq ($(CHIP),t13a)
+    LFUSE = 0x6A
+    HFUSE = 0xFF
+    EFUSE = 0xFF # Not used on t13a
+else
+    LFUSE = 0x62
+    HFUSE = 0xDA
+    EFUSE = 0xFF
+endif
 
+# Default build rules
 all: $(TARGET).hex size
 
 $(TARGET).elf: $(TARGET).c
@@ -43,10 +56,17 @@ size: $(TARGET).elf
 	avr-size --format=avr --mcu=$(MCU) "$<"
 
 fuses:
+ifeq ($(CHIP),t13a)
+	$(AVRDUDE) -c $(PROGRAMMER) -p $(AVRDUDE_MCU) -B 40 \
+		-U lfuse:w:$(LFUSE):m \
+		-U hfuse:w:$(HFUSE):m
+else
 	$(AVRDUDE) -c $(PROGRAMMER) -p $(AVRDUDE_MCU) -B 40 \
 		-U lfuse:w:$(LFUSE):m \
 		-U hfuse:w:$(HFUSE):m \
 		-U efuse:w:$(EFUSE):m
+endif
+
 
 readfuses:
 	$(AVRDUDE) -c $(PROGRAMMER) -p $(AVRDUDE_MCU) -B 40 \
--- main.c
+++ main.c
@@ -1,61 +1,83 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-void power_save(void){
-
-	PRR |= (1 << PRTWI); //Disable two wire
-	PRR |= (1 << PRTIM2); //Disable Timer/Counter 2
-	PRR |= (1 << PRSPI); //Disable SPI
-	PRR |= (1 << PRUSART0); //Disable USART
+void power_save(void) {
+    #ifdef __AVR_ATtiny13A__
+        // No PRR on ATtiny13A, so this function does nothing for it
+    #elif defined(__AVR_ATmega328P__)
+        PRR |= (1 << PRTWI);    // Disable two-wire interface (TWI)
+        PRR |= (1 << PRTIM2);   // Disable Timer/Counter 2
+        PRR |= (1 << PRSPI);    // Disable SPI
+        PRR |= (1 << PRUSART0); // Disable USART
+    #else
+        #error "Unsupported microcontroller!"
+    #endif
 }
 
-void pwm_init(void){
-
-	DDRB |= (1 << PB1); //PB1 as output
-	
-	TCCR1A |= (1 << WGM10); //Fast PWM, 8 bit
-	TCCR1A |= (1 << COM1A1); //Non-inverting mode on OC1A
-	TCCR1B |= (1 << WGM12); //Fast PWM, 8 bit
-	TCCR1B |= (1 << CS11); //Prescaler=8
-	
-	OCR1A = 0; //Duty cycle 0, motor off
+void pwm_init(void) {
+    #ifdef __AVR_ATtiny13A__
+        DDRB |= (1 << PB0); // PB0 as output (OC0A)
+        TCCR0A |= (1 << WGM00) | (1 << WGM01); // Fast PWM, 8-bit
+        TCCR0A |= (1 << COM0A1);               // Non-inverting mode on OC0A
+        TCCR0B |= (1 << CS01);                 // Prescaler = 8
+        OCR0A = 0;                             // Duty cycle 0, motor off
+    #elif defined(__AVR_ATmega328P__)
+        DDRB |= (1 << PB1); // PB1 as output (OC1A)
+        TCCR1A |= (1 << WGM10); // Fast PWM, 8-bit
+        TCCR1A |= (1 << COM1A1); // Non-inverting mode on OC1A
+        TCCR1B |= (1 << WGM12); // Fast PWM, 8-bit
+        TCCR1B |= (1 << CS11); // Prescaler = 8
+        OCR1A = 0; // Duty cycle 0, motor off
+    #else
+        #error "Unsupported microcontroller!"
+    #endif
 }
 
-void adc_setup(void){
-	ADMUX = (ADMUX | (1<<REFS1)) | (1 << REFS0);
-	DDRB &= ~(1 << PB3); // PB3 as input
-	DIDR0 |= (1 << ADC3D); // Disable PB3 digital input
-	ADMUX = (1 << MUX1) | (1 << MUX0); // Select ADC3
-	ADCSRA = (1 << ADEN) | (1 << ADPS1) | (1 << ADPS0); //Enable ADC, set prescaler to 8
-	
+void adc_setup(void) {
+    #ifdef __AVR_ATtiny13A__
+        DDRB &= ~(1 << PB3);            // PB3 as input
+        DIDR0 |= (1 << ADC3D);          // Disable digital input on PB3
+        ADMUX = (1 << MUX1) | (1 << MUX0); // Select ADC3 (PB3)
+        ADCSRA = (1 << ADEN) | (1 << ADPS1) | (1 << ADPS0); // Enable ADC, prescaler = 8
+    #elif defined(__AVR_ATmega328P__)
+        DDRC &= ~(1 << PC0);            // PC0 as input
+        DIDR0 |= (1 << ADC0D);          // Disable digital input on PC0
+        ADMUX = (1 << REFS0);           // AVcc as reference, select ADC0 (PC0)
+        ADCSRA = (1 << ADEN) | (1 << ADPS1) | (1 << ADPS0); // Enable ADC, prescaler = 8
+    #else
+        #error "Unsupported microcontroller!"
+    #endif
 }
 
-uint16_t read_adc(void){
-	ADCSRA |= (1 << ADSC);
-	while(ADCSRA & (1 << ADSC));
-	return ADC;
-}
-void set_duty(uint8_t duty){
-
-	OCR1A = duty;
+uint16_t read_adc(void) {
+    ADCSRA |= (1 << ADSC);              // Start ADC conversion
+    while (ADCSRA & (1 << ADSC));       // Wait for conversion to complete
+    return ADC;                         // Return ADC result
 }
 
-int main(void){
-	
-	power_save();
-	pwm_init();
-	adc_setup();
-
-	while(1){
-		for(uint8_t i = 0; i<255; i++){
-			set_duty(i);
-			_delay_ms(10);
-		}
-		for (uint8_t i = 255; i> 0; i--){
-			set_duty(i);
-			_delay_ms(10);
-		}
-	}
-
+void set_duty(uint8_t duty) {
+    #ifdef __AVR_ATtiny13A__
+        OCR0A = duty; // Set duty cycle for Timer0 (OC0A)
+    #elif defined(__AVR_ATmega328P__)
+        OCR1A = duty; // Set duty cycle for Timer1 (OC1A)
+    #else
+        #error "Unsupported microcontroller!"
+    #endif
 }
 
+int main(void) {
+    power_save();
+    pwm_init();
+    adc_setup();
+
+    while (1) {
+        for (uint8_t i = 0; i < 255; i++) {
+            set_duty(i);
+            _delay_ms(10);
+        }
+        for (uint8_t i = 255; i > 0; i--) {
+            set_duty(i);
+            _delay_ms(10);
+        }
+    }
+}
